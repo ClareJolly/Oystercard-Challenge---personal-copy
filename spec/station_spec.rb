9c3ee1f62c35e700bcce451d23cabe9529fed0d0
Clare Jolly <clarejollyuk@gmail.com>
2018-10-09 17:18:32 +0100

    small updates

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,7 +1,13 @@
 require "station"
 
 describe Station do
+
   it "has a zone allocated to it" do
     expect(Station.new("Camden").get_zone).to eq 1
   end
+
+  it "has a name allocated to it" do
+    expect(Station.new("Camden").get_name).to eq "Camden"
+  end
+  
 end
